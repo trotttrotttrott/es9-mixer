624621cda62a2d07b0473470ba74547897e8102e
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2021-05-16 13:32:27 -0600

    MixDump sets mixes

--- src/MIDI/Message/MixDump.js
+++ src/MIDI/Message/MixDump.js
@@ -1,6 +1,19 @@
 class MixDump {
 
   static id = 0x11;
+
+  constructor(data) {
+    var c = 390;
+    this.mixes = Array(16).fill(0).map(function(_, mix) {
+      return Array(8).fill(0).map(function(_, channel) {
+        var mix = {
+          volume: data[c],
+        };
+        c += 2;
+        return mix;
+      });
+    });
+  }
 }
 
 export default MixDump;
