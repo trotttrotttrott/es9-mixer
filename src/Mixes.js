f5880c91b4ee6ad240c9ac9a14ebdd0627b1f5fc
Chris Trott <908409+trotttrotttrott@users.noreply.github.com>
2021-05-16 16:41:48 -0600

    Cleanup styles

--- src/Mixes.js
+++ src/Mixes.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import Mix from './Mix';
+import './Mixes.css';
 
 class Mixes extends React.Component {
 
@@ -11,20 +13,23 @@ class Mixes extends React.Component {
         return '';
       } else {
         return (
-          <Mix
-            key={i}
-            number={i}
-            channels={e}
-            disableChannels={this.props.disableChannels[i + 1] || []}
-            updateVolume={this.props.updateVolume}
-          />
+          <Grid item key={i} xs={3}>
+            <Mix
+              number={i}
+              channels={e}
+              disableChannels={this.props.disableChannels[i + 1] || []}
+              updateVolume={this.props.updateVolume}
+            />
+          </Grid>
         )
       }
     }.bind(this));
 
     return (
       <div className="Mixes">
-        {mixes}
+        <Grid container spacing={1}>
+          {mixes}
+        </Grid>
       </div>
     )
   }
